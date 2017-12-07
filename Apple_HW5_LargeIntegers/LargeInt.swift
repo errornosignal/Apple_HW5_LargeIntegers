a5995df4ad3501938fb0418f9feec6ce7466318d
errornosignal <errornosignal@users.noreply.github.com>
2017-12-07 01:15:50 -0600

    day1

---
+++ Apple_HW5_LargeIntegers/LargeInt.swift
@@ -0,0 +1,13 @@
+//
+//  LargeInt.swift
+//  Apple_HW5_LargeIntegers
+//
+//  Created by Reid Nolan on 12/7/17.
+//  Copyright © 2017 Reid Nolan. All rights reserved.
+//
+
+import Foundation
+
+class LargeInt {
+    let mInt = " "
+}
